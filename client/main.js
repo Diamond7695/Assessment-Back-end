54b86be884ba3c56dba0a18e3ff0d38d71dbdeff
Diamond Toliver <diamondtoliver@Diamonds-MacBook-Pro.local>
2021-12-06 00:00:17 -0500

    added Two new Buttons

--- client/main.js
+++ client/main.js
@@ -16,7 +16,15 @@ axios.get("http://localhost:4000/api/furtune")
   });
 };  
 
-
+function show_date() { 
+  var in_date = new Date();
+  var str=in_date.getDate() +'/'+(in_date.getMonth()+1)+'/'+in_date.getFullYear()
+  document.getElementById('display2').innerHTML=str
+  }
+  
+  function changeBack() {
+    document.body.style.background = "url('https://wallpaperaccess.com/full/6043682.jpg') no-repeat right top";
+  }
 
 
 
